3ee63efb227ea00e7a80b9ede5a1373f85b6c90a
Kyle Permuy <kyle@varninja.com>
2017-09-23 22:14:25 -0400

    Adding helper functions for 'Add Song' event #4

--- events/play.js
+++ events/play.js
@@ -1,12 +1,11 @@
-module.exports = (rt, socket) => {
+const _ = require('underscore');
+const Promise = require('bluebird');
+const mongoose = require('mongoose');
 
-  /**
-   * Grab the song object from our database.
-   * @param  {[type]} data [description]
-   * @return {[type]}      [description]
-   */
-  const checkSongFromDb = (data) => {
-  }
+const Mix = mongoose.models.Mix;
+const Song = mongoose.models.Song;
+
+module.exports = (rt, socket) => {
 
   /**
    * If a song doesn't exist in our Song collection in MongoDB,
@@ -15,6 +14,36 @@ module.exports = (rt, socket) => {
    * @return {[type]}      [description]
    */
   const initializeSong = (data) => {
+    new Song(data)
+    .save()
+    .then((saveRes) => {
+      console.log(saveRes)
+      return saveRes
+    })
+    .catch((err) => {
+      console.log("initializeSong Error: " err)
+      return err
+    })
+  }
+
+  /**
+   * Grab the song object from our database. Check to make sure it exists first,
+   * if not, run initializeSong
+   * @param  {[type]} data [description]
+   * @return {[type]}      [description]
+   */
+  const grabSongFromDb = (data) => {
+    return new Promise((resolve, reject) => {
+      Song.findOne({"source.provider": data.provider, "source.songId": data.songId})
+      .then((fetchedSong) => {
+        console.log(fetchedSong);
+        return resolve(fetchedSong)
+      })
+      .catch((err) => {
+        console.log(err)
+        return reject(err)
+      })
+    })
   }
 
   /**
@@ -23,6 +52,8 @@ module.exports = (rt, socket) => {
    * @param {Function} callback [description]
    */
   const addSong = (data, callback) => {
+    //let songTitle = data.title
+
   }
 
   /**
@@ -32,6 +63,7 @@ module.exports = (rt, socket) => {
    * @return {[type]}            [description]
    */
   const removeSong = (data, callback) => {
+
   }
 
   /**
@@ -42,6 +74,7 @@ module.exports = (rt, socket) => {
    * @return {[type]}            [description]
    */
   const editOrder = (data, callback) => {
+
   }
 
   return {addSong, removeSong, editOrder}
