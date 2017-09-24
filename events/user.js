45a47382a44e0a882509a1180b8a94659c43abe5
desmondhow <desmondhow.dh@gmail.com>
2017-09-23 20:25:31 -0400

    added updateUserList to send back current list of users in mix to client

--- events/user.js
+++ events/user.js
@@ -6,68 +6,76 @@ const User = mongoose.models.UserModel;
 
 module.exports = (rt, socket) => {
 
+  const initializeUser = (data, callback) => {
+    let initUser = new User({ "current_socket": socket.id });
+    initUser.save().then((initRes) => {
+      console.log(initRes);
+      socket.emit('set mongo id', initRes._id);
+    })
+  }
+
   const auth = (data, callback) => {
     console.log("auth running");
     // data = Jason.parse(data);
     let userId = data.id; // do something to ID
     console.log(userId);
     User.find({ "fb_id" : userId })
-    .then((response) => {
-      if (!_.isEmpty(response)) {
-        User.update(response._id, { "current_socket": socket.id },
-        (err, updateRes) => {
-          console.log(err);
-          console.log(updateRes);
-          return callback({
-            status: true,
-            user: {
-              name: response.name,
-              fb_id: response.fb_id
+      .then((response) => {
+        if (!_.isEmpty(response)) {
+          User.update(response._id, { "current_socket": socket.id },
+            (err, updateRes) => {
+              console.log(err);
+              console.log(updateRes);
+              return callback({
+                status: true,
+                user: {
+                  name: response.name,
+                  fb_id: response.fb_id
+                }
+              });
+            });
+        }
+        else {
+          console.log("nooooooo");
+          console.log(data.token);
+          // create a new user object
+          let user = new User({
+            current_socket: socket.id,
+            name: data.name,
+            email: data.email,
+            fb_id: data.id,
+            fb_token: data.token,
+          });
+          user.save((err, saveRes) => {
+            if (err) callback({ status: false });
+            console.log(saveRes);
+            if (saveRes.ok == 1) {
+              return callback({
+                status: true,
+                user: {
+                  name: response.name,
+                  fb_id: response.fb_id
+                }
+              });
+            }
+            else {
+              return callback({
+                status: false
+              });
             }
           });
-        });
-      }
-      else {
-        console.log("nooooooo");
-        console.log(data.token);
-        // create a new user object
-        let user = new User({
-          current_socket: socket.id,
-          name: data.name,
-          email: data.email,
-          fb_id: data.id,
-          fb_token: data.token,
-        });
-        user.save((err, saveRes) => {
-          if (err) callback({ status: false });
-          console.log(saveRes);
-          if (saveRes.ok == 1) {
-            return callback({
-              status: true,
-              user: {
-                name: response.name,
-                fb_id: response.fb_id
-              }
-            });
-          }
-          else {
-            return callback({
-              status: false
-            });
-          }
-        });
-      }
-    });
+        }
+      });
   }
 
   const logout = (callback) => {
     User.update({ "current_socket" : socket.id }, { "current_socket": "" })
     .then((updateRes) => {
-      console.log("updateRes on logout: " + updateRes);
-      return callback({
-        status: true
+        console.log("updateRes on logout: " + updateRes);
+        return callback({
+          status: true
+        })
       })
-    })
   }
 
   return { auth, logout };
